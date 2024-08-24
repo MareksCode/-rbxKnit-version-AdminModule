ed3681ea3f9f0b3290852e710c3352c3b1f0bbdf
MareksCode <marcwenzel4@gmail.com>
2024-08-24 03:08:49 +0200

    changed KnitInit to Knitstart

--- src/StarterPlayer/ModerationController.lua
+++ src/StarterPlayer/ModerationController.lua
@@ -2,21 +2,21 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
 local ModerationController = Knit.CreateController { Name = "ModerationController" }
-local ModerationService
+local ModerationService, NotificationController
 
 function ModerationController:KnitStart()
-    ModerationService = Knit.GetService("ModerationService")
+    local function CommandFeedbackEventFunction(text)
+        NotificationController:DisplayNotification(text, "rbxassetid://87731268040856", 30, nil, "Moderation")
+       -- print("ModerationController: "..text)
+    end
+
+    ModerationService.CommandFeedbackEvent:Connect(CommandFeedbackEventFunction)
 end
 
 
 function ModerationController:KnitInit()
-    local function CommandFeedbackEvent(text)
-        --add your preferred method of notification here
-
-        print("ModerationController: "..text)
-    end
-
-    ModerationService.CommandFeedbackEvent:Connect(CommandFeedbackEvent)
+    ModerationService = Knit.GetService("ModerationService")
+    NotificationController = Knit.GetController("NotificationController")
 end
 
 
